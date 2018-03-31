43a6d94efe36e8296d51b734072f47c810e00b19
Prashant Chaudhary <101.prashant@gmail.com>
2018-04-01 00:05:26 +0530

    control ball with key presses

--- Drawing/src/codekamp/Demo.java
+++ Drawing/src/codekamp/Demo.java
@@ -2,16 +2,29 @@ package codekamp;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Demo {
+public class Demo implements KeyListener {
+    private int xVel = 2;
+    private int yVel = 0;
+
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(800, 450);
 
         JPanel p = new JPanel();
+        p.setPreferredSize(new Dimension(800, 450));
+        p.setFocusable(true);
+
+        Demo d1 = new Demo();
+
+        p.addKeyListener(d1);
+
         frame.add(p);
 
+        frame.pack();
         frame.setVisible(true);
+
 
         try {
             Thread.sleep(200);
@@ -19,31 +32,53 @@ public class Demo {
 
         }
 
-        Graphics g = p.getGraphics();
-//
-//        g.setColor(Color.RED);
-//        g.fillRect(10, 100, 50, 150);
+        p.requestFocus();
 
+        Graphics g = p.getGraphics();
         g.setColor(Color.GREEN);
 
 
         int x = 0;
         int y = 100;
 
-        int xVel = 2;
-        int yVel = 2;
         while (true) {
             try {
                 Thread.sleep(25);
             } catch (InterruptedException e) {
-
             }
 
-            x += xVel;
-            y += yVel;
+            x += d1.xVel;
+            y += d1.yVel;
 
             g.clearRect(0, 0, 800, 450);
             g.fillArc(x, y, 100, 100, 0, 360);
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+            this.xVel = 0;
+            this.yVel = 2;
+        } else if(e.getKeyCode() == KeyEvent.VK_UP) {
+            this.xVel = 0;
+            this.yVel = -2;
+        } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+            this.xVel = -2;
+            this.yVel = 0;
+        } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            this.xVel = 2;
+            this.yVel = 0;
+        } else {
+            System.out.println("Some faltu key pressed");
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
     }
 }
